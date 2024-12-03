d294e252c3827e495b75e76a346c3ce5c317a235
Arina Riabukha <arrin.uq@gmail.com>
2024-12-03 14:05:32 +0200

    js files

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,16 +1,25 @@
-export function getPhotos(img) {
-    const API_KEY= "47393342-0b139bb531c328d079618af8b";
-    const URL= "https://pixabay.com/api/";
-    return fetch(`${URL}?key=${API_KEY}&q=${encodeURIComponent(img)}&image_type=photo&orientation=horizontal&safesearch=true`)
-        .then(response=> {
+const getPhotos = (img) => {
+    const API_KEY = "47393342-0b139bb531c328d079618af8b";
+    const searchParams = new URLSearchParams({
+        key: API_KEY,
+        q: img,
+        image_type: "photo",
+        orientation: "horizontal",
+        safesearch: true,
+    });
+
+    return fetch(`https://pixabay.com/api/?${searchParams}`)
+        .then((response) => {
             if (!response.ok) {
-                throw new Error(`Error status: ${response.status}`);
+                throw new Error(`Error: ${response.status}`);
             }
             return response.json();
         })
-        .then(data=> data)
-        .catch(error=> {
-            console.error("Error fetching:", error);
-            throw error; 
+        .then((data) => data.hits)
+        .catch((error) => {
+            console.error("Failed to fetch photos:", error);
+            return [];
         });
-}
+};
+
+export default getPhotos;
